64ebd0c6eb3abdbecb20797125cd5b4c38c96f3b
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-07 17:01:38 +0530

    Write the test for basic test

---
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+void main(){
+  final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test');
+
+  test('should be a subclass of NumberTriviaEntity entity', ()async{
+    // assert
+
+    expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+}
