2cf99deee1b1373d6b24cbbea474d70dd8d80b14
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-07 17:20:17 +0530

    Write test

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:numbertrivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
 
 void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test');
@@ -9,5 +13,17 @@ void main() {
     // assert
 
     expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+
+  group("From JSON ", () async {
+    test("Should return valid model whn the JSON number is an integer ",
+        () async {
+      // arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia.json'));
+      // act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      // assert
+      expect(result, tNumberTriviaModel);
+    });
   });
 }
