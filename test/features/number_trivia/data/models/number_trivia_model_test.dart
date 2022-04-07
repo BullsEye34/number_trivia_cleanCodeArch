2e2021f5fab120ab1c7730ec2572df3492a441d0
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-07 17:04:17 +0530

    Create subClass

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:numbertrivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
 
 void main(){
