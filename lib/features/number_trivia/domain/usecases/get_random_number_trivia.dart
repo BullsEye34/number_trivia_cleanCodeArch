43cd21a56cb2e4b50a4b5712f2a5c17631e950af
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-07 17:05:20 +0530

    Fix Imports

--- lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:numbertrivia/core/error/failures.dart';
 import 'package:numbertrivia/core/usecases/usecase.dart';
 import 'package:numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
-import 'package:numbertrivia/features/number_trivia/domain/repositories/number_trivia_repositories.dart';
+import 'package:numbertrivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
 
 class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams>{
   late final NumberTriviaRepository numberTriviaRepository;
