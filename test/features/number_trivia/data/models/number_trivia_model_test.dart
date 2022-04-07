97a51582b54837f7bdf9ad8d64080e13a3ce51f4
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-07 17:10:05 +0530

    Indent

--- test/features/number_trivia/data/models/number_trivia_model_test.dart
+++ test/features/number_trivia/data/models/number_trivia_model_test.dart
@@ -2,10 +2,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:numbertrivia/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
 
-void main(){
+void main() {
   final tNumberTriviaModel = NumberTriviaModel(number: 1, text: 'test');
 
-  test('should be a subclass of NumberTriviaEntity entity', ()async{
+  test('should be a subclass of NumberTriviaEntity entity', () async {
     // assert
 
     expect(tNumberTriviaModel, isA<NumberTrivia>());
