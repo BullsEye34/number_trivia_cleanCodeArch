514ac375289ca78ba2b17468e92899df403fdca3
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-08 11:19:31 +0530

    Exceptions

--- lib/core/error/failures.dart
+++ lib/core/error/failures.dart
@@ -1,7 +1,11 @@
 import 'package:equatable/equatable.dart';
 
-abstract class Failure extends Equatable with EquatableMixin{
-  
+abstract class Failure extends Equatable with EquatableMixin {
   @override
-  List<Object?> get props=>[];
+  List<Object?> get props => [];
 }
+
+// failues
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
