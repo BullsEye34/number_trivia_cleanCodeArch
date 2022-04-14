c2af1f60a34d0909ba5b725e36a3ef24fc60be97
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-14 22:14:21 +0530

    Write initial State test and impl

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,14 +1,27 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:numbertrivia/core/util/input_converter.dart';
 import 'package:numbertrivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:numbertrivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:numbertrivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
-  NumberTriviaBloc() : super(Empty()) {
+  late final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  late final GetRandomNumberTrivia getRandomNumberTrivia;
+  late final InputConverter inputConverter;
+  NumberTriviaBloc({
+    required this.getConcreteNumberTrivia,
+    required this.getRandomNumberTrivia,
+    required this.inputConverter,
+  }) : super(Empty()) {
     on<NumberTriviaEvent>((event, emit) {
       // TODO: implement event handler
     });
   }
+
+  @override
+  NumberTriviaState get initialState => Empty();
 }
---
+++ test/features/number_trivia/presentation/bloc/number_trivia_bloc_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:numbertrivia/core/util/input_converter.dart';
+import 'package:mockito/mockito.dart';
+import 'package:numbertrivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:numbertrivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:numbertrivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+
+class MockGetConcreteNumberTrivia extends Mock
+    implements GetConcreteNumberTrivia {}
+
+class MockGetRandomNumberTrivia extends Mock implements GetRandomNumberTrivia {}
+
+class MockInputConverter extends Mock implements InputConverter {}
+
+void main() {
+  late GetConcreteNumberTrivia mockGetConcreteNumberTrivia;
+  late GetRandomNumberTrivia mockGetRandomNumberTrivia;
+  late InputConverter mockInputConverter;
+  late NumberTriviaBloc bloc;
+  setUp(() {
+    mockGetConcreteNumberTrivia = MockGetConcreteNumberTrivia();
+    mockGetRandomNumberTrivia = MockGetRandomNumberTrivia();
+    mockInputConverter = MockInputConverter();
+    bloc = NumberTriviaBloc(
+      getConcreteNumberTrivia: mockGetConcreteNumberTrivia,
+      getRandomNumberTrivia: mockGetRandomNumberTrivia,
+      inputConverter: mockInputConverter,
+    );
+  });
+
+  test('initialState should be Empty', () {
+    // assert
+    expect(bloc.initialState, equals(Empty()));
+  });
+}
