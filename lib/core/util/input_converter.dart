065579311eb5e71cc5de36b69517cd9c438820d5
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-14 20:53:49 +0530

    Write class

---
+++ lib/core/util/input_converter.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:numbertrivia/core/error/failures.dart';
+
+class InpuConverter {
+  Either<Failure, int>? stringToUnignedInteger(String str) {
+    throw UnimplementedError();
+  }
+}
