3dcd51d08dd43f22f48244407516661b45ab657b
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-14 18:51:17 +0530

    Write Failing test

--- test/features/number_trivia/data/datasources/number_trivia_remote_datasource_test.dart
+++ test/features/number_trivia/data/datasources/number_trivia_remote_datasource_test.dart
@@ -3,6 +3,8 @@ import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
 import 'package:numbertrivia/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
 
+import '../../../../fixtures/fixture_reader.dart';
+
 class MockHttpClient extends Mock implements http.Client {}
 
 void main() {
@@ -12,5 +14,30 @@ void main() {
   setUp(() {
     mockHttpClient = MockHttpClient();
     dataSource = NumberTriviaRemoteDatasourceImpl(client: mockHttpClient);
+  });
+
+  group("Get Concrete Number Trivia", () {
+    final tnumber = 1;
+    test(
+        "Should perform GET request on URL with tNumber being enpoint and with application.json header",
+        () async {
+      // arrange
+      when(
+        mockHttpClient.get(
+          Uri.parse("http://numbersapi.com/$tnumber"),
+          headers: anyNamed('headers'),
+        ),
+      ).thenAnswer(
+          (realInvocation) async => http.Response(fixture("trivia.json"), 200));
+      // act
+      dataSource.getConcreteNumberTrivia(tnumber);
+      // assert
+      verify(
+        mockHttpClient.get(
+          Uri.parse("http://numbersapi.com/$tnumber"),
+          headers: {'Content-Type': 'application/json'},
+        ),
+      );
+    });
   });
 }
