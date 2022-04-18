7e543a8fccd222a01889249487aaa9ef9c6fe0eb
P Vamshi Prasad <vamvijkum@gmail.com>
2022-04-18 18:29:49 +0530

    Write the Dependencies for injectyion

---
+++ lib/injection_container.dart
@@ -0,0 +1,57 @@
+import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:numbertrivia/core/network/network_information.dart';
+import 'package:numbertrivia/core/util/input_converter.dart';
+import 'package:numbertrivia/features/number_trivia/data/datasources/number_trivia_local_datasource.dart';
+import 'package:numbertrivia/features/number_trivia/data/datasources/number_trivia_remote_datasource.dart';
+import 'package:numbertrivia/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:numbertrivia/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:numbertrivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:numbertrivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:numbertrivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+void init() {
+  //! Features - Number Trivia
+  // Register BLOC
+  sl.registerFactory(
+    () => NumberTriviaBloc(
+      getConcreteNumberTrivia: sl(),
+      getRandomNumberTrivia: sl(),
+      inputConverter: sl(),
+    ),
+  );
+
+  // Register Use Cases
+  sl.registerLazySingleton(() => GetConcreteNumberTrivia(sl()));
+  sl.registerLazySingleton(() => GetRandomNumberTrivia(sl()));
+
+  // Register Repository
+  sl.registerLazySingleton<NumberTriviaRepository>(
+    () => NumberTriviaRepositoryImplementation(
+      remoteDataSource: sl(),
+      localDataSource: sl(),
+      networkInformation: sl(),
+    ),
+  );
+  // Datasource
+  sl.registerLazySingleton<NumberTriviaLocalDatasource>(
+    () => NumberTriviaLocalDataSourceImpl(sharedPreferences: sl()),
+  );
+  sl.registerLazySingleton<NumberTriviaRemoteDatasource>(
+    () => NumberTriviaRemoteDatasourceImpl(client: sl()),
+  );
+
+  //! Core
+  sl.registerLazySingleton(() => InputConverter());
+  sl.registerLazySingleton<NetworkInformation>(
+      () => NetworkInformationImpl(sl()));
+
+  //! Extrenal
+  sl.registerLazySingletonAsync(() => SharedPreferences.getInstance());
+  sl.registerLazySingleton(() => http.Client());
+  sl.registerLazySingleton(() => InternetConnectionChecker());
+}
